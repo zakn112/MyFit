d476062564b0d0c1e50497cd27431ae297b711c5
zakn112 <56603472+zakn112@users.noreply.github.com>
2020-08-31 19:40:03 +0300

    Initial commit

---
+++ MyFit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFit
+//
+//  Created by Андрей Закусов on 31.08.2020.
+//  Copyright © 2020 Андрей Закусов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
